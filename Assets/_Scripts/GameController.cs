a363358a418a1349c991987d4238bf07ec7ba53e
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-09-30 10:00:40 -0400

    added Game Controller

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    // FIELDS
+    public GameObject Cloud;
+    public int cloudNumber;
+
+    public List<GameObject> clouds;
+
+
+	// Use this for initialization
+	void Start () {
+        this.clouds = new List<GameObject>();
+        for (int count = 0; count < this.cloudNumber; count++)
+        {
+            this.clouds.Add(Instantiate(Cloud));
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
